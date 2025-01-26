8e858fe15e8507e10a9efa40342d9a93c5796a03
Saket <saketruia18@gmail.com>
2025-01-26 21:37:03 +0530

    backend is connected to dailogflow

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import JSONResponse
+
 
 app=FastAPI()
 
-@app.get("/")
+@app.post("/")
 
-async def root():
-    return {"message":"Hello World"}
+async def handle_request( request: Request):
+    payload =await request.json()
+
+    intent = payload['queryResult']['intent']['displayName']
+    parameters =payload['queryResult']['parameters']
+    output_contexts=payload['queryResult']['outputContexts']
+
+
+    if intent=="track.order - context: ongoing-tracking":
+        return JSONResponse(content={
+            "fulfillmentText": f"Received =={intent}== in the backend"
+        })
