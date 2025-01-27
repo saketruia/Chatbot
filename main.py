660fc46ba577a22e825b5b33ea4fede076c1fddc
Saket <saketruia18@gmail.com>
2025-01-27 23:08:08 +0530

    intent handler

--- main.py
+++ main.py
@@ -14,9 +14,13 @@ async def handle_request( request: Request):
     parameters =payload['queryResult']['parameters']
     output_contexts=payload['queryResult']['outputContexts']
 
+    intent_handler_dict = {
+        'order.add - context: ongoing-order': add_to_order,
+        'order.remove - context: ongoing-order': remove_from_order,
+        'order.complete - context: ongoing-order': complete_order,
+        'track.order - context: ongoing-tracking': track_order
+    }
 
-    if intent=="track.order - context: ongoing-tracking":
-        return track_order(parameters)
     
 
 def track_order(parameters: dict, session_id: str):
